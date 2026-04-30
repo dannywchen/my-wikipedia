e5def91ddb8f24d9013561a8f1ff5c2a49dc342f
Danny Chen <dannywchenofficial@gmail.com>
2026-04-30 00:42:23 -0400

    Quartz sync: Apr 30, 2026, 12:42 AM

---
+++ content/templates/Note Template.md
@@ -0,0 +1,11 @@
+---
+class:
+  - note
+created_on: "[[<% tp.date.now('MM-DD-YYYY') %>]]"
+tags:
+source:
+related:
+author:
+description:
+aliases:
+---
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,17 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        repo: "dannywchen/my-wikipedia",
+        repoId: "R_kgDOSQbkOA",
+        category: "Announcements",
+        categoryId: "DIC_kwDOSQbkOM4C8Ak-",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/dannywchen",
