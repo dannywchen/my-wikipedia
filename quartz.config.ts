14538090cc1a3935149546d15203191e571ef110
Danny Chen <dannywchenofficial@gmail.com>
2026-04-30 01:14:55 -0400

    new config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dannywiki.vercel.app", 
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
